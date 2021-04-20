45b8f4161a9a19ebae8e3e72011b51269b11a579
Haden Jackson-Robbins <haden.jackson-robbins@spike95.co.uk>
2021-04-20 10:46:30 +0100

    Minor comments

--- src/com/javaxmlexample/Main.java
+++ src/com/javaxmlexample/Main.java
@@ -16,8 +16,15 @@ public class Main {
 
     public static void main(String[] args) throws ParserConfigurationException, TransformerException
     {
-	    // Source material: https://www.tutorialspoint.com/java_xml/java_dom_create_document.htm
+	    // Source material:
+        //  https://www.tutorialspoint.com/java_xml/java_dom_create_document.htm
+        //  https://en.wikipedia.org/wiki/Builder_pattern
+        //  https://howtodoinjava.com/design-patterns/creational/builder-pattern-in-java/
+
         boolean exportToFile = false;
+
+        // todo - Set up the car collection prior to creating the CarCollectionBuilder below? Otherwise you get duplication
+        // todo - which isn't a good look??? or not?!
 
         Document carCollectionDoc = CarCollectionBuilder
                 .create()
@@ -33,19 +40,25 @@ public class Main {
 
     }
 
+    // todo - create an XML utilities folder and this and the writeXMLtoconsole method in it
+
     private static void exportToFile(Document carCollectionDoc) throws TransformerException {
+
         /////////////////////////////////////////////////////////////////////////
         // write the content into xml file (writes into top level project folder
         /////////////////////////////////////////////////////////////////////////
+
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         DOMSource source = new DOMSource(carCollectionDoc);
+
         // Write the file to the present working directory which should be the top level folder of this project
         StreamResult result = new StreamResult(new File("cars.xml"));
         transformer.transform(source, result);
     }
 
     private static void writeXMLDocumentToConsole(Document carCollectionDoc) throws TransformerException {
+
         System.out.println("-------------------------------------------------------------------------\n");
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////
